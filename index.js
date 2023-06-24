83a313f531f3d9cda4ddd22cd0556a91573f64a6
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-24 19:47:47 +0300

    done with db.json index.html and style.css, created a new image folder

---
+++ index.js
@@ -0,0 +1,31 @@
+function fetchData() {
+    fetch('http://localhost:3000/characters')
+    .then(res => res.json())
+    .then(data => data.forEach((characters) => theInfo(characters)))
+}
+
+function theInfo(characters) {
+    const li = document.createElement('li')
+    li.className ="card"
+    li.innerHTML =
+    `<p>${characters.name}</p>
+    <div>
+    <img src="${characters.image}">
+    <p>${characters.id}</p>
+    <p>Votes: ${characters.votes}</p>
+    </div>
+    <br>
+    `
+    document.querySelector('ul').appendChild(li)
+}
+
+fetchData()
+
+document.addEventListener("DOMContentLoaded", () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        theInfo(e.target.input.value)
+        form.reset()
+    })
+})
