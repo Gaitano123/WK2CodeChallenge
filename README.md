949f8aa6f3bb1291faec0c815cbccc6145babeb9
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-25 17:46:09 +0300

    README remaining

---
+++ README.md
@@ -0,0 +1,14 @@
+# **`WEEK TWO CODE CHALLENGE`**
+
+## **`INTRODUCTION`**
+In this code challenge I was working on Flataculties, this is a programm which allows the user to vote for the cutest animal!
+
+I used a local API and build out the frontend for our app, Flataculties. The files I used for the program are
+* index.html
+* style.css
+* index.js
+
+---
+
+## **`DESCRIPTION`**
+
--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 function fetchData() {
     fetch('http://localhost:3000/characters')
-    .then(res => res.json())
-    .then(data => data.forEach((characters) => theInfo(characters)))
-}
-
-function theInfo(characters) {
-    const li = document.createElement('li')
-    li.className ="card"
-    li.innerHTML =
-    `<p>${characters.name}</p>
-    <div>
-    <img src="${characters.image}">
-    <p>${characters.id}</p>
-    <p>Votes: ${characters.votes}</p>
-    </div>
-    <br>
-    `
-    document.querySelector('ul').appendChild(li)
-}
-
-fetchData()
+      .then(res => res.json())
+      .then(data => data.forEach(character => createCard(character)));
+  }
+  
+  function createCard(character) {
+    const li = document.createElement('li');
+    li.className = "card";
+    li.innerHTML = `
+      <p>${character.name}</p>
+      <div>
+        <img src="${character.image}">
+        <p>ID: ${character.id}</p>
+        <p>Votes: <span class="votes">${character.votes}</span></p>
+        <button class="voting">Vote</button>
+      </div>
+      <br>
+    `;
+  
+    const button = li.querySelector('button');
+    button.addEventListener('click', () => {
+      voteForCharacter(button, character);
+    });
+  
+    document.querySelector('ul').appendChild(li);
+  }
+  
+  function voteForCharacter(button, character) {
+    button.removeEventListener('click', voteForCharacter);
+    button.disabled = true;
+  
+    // Simulating the voting by incrementing the votes count
+    character.votes++;
+    const votesElement = button.parentNode.querySelector('.votes');
+    votesElement.textContent = character.votes;
+  }
+  
+  fetchData();
+  
 
 document.addEventListener("DOMContentLoaded", () => {
     let form = document.querySelector('form')
@@ -29,16 +47,3 @@ document.addEventListener("DOMContentLoaded", () => {
         form.reset()
     })
 })
-
-function trial (){
-    const list = document.createElement('li')
-    list.className = 'list'
-    list.innerHTML = `
-    <p>${characters.name}</p>
-    <div>
-    <img src="${characters.image}">
-    <p>${characters.id}</p>
-    <p>Votes: ${characters.votes}</p>
-    </div>
-    `
-}
